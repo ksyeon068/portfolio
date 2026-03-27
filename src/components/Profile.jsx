2e7d4cbe0993007ab6a4fcf18bdf25977fb5213c
seyeon <ksyeon068@gmail.com>
2026-03-27 18:00:27 +0900

    프로젝트만지는중

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/profile.scss';
 
 const Profile = () => {
     return (
@@ -6,7 +7,9 @@ const Profile = () => {
             <div className="profile-left">
                 <img src="./" alt="프로필용이미지" />
                 <span>김세연</span>
-                <p></p>
+                <p>사용자 경험을 중심으로 디자인하고, 이를 코드로 구현하는 김세연입니다.</p>
+                <br />
+                <p>시각적인 완성도와 인터랙션의 흐름을 함께 고민하며, 디자인과 개발의 경계를 자연스럽게 연결합니다.</p>
             </div>
             <div className="profile-right"></div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+import '../style/projects.scss';
+
+import { Mousewheel, Pagination, FreeMode, Navigation, Thumbs} from 'swiper/modules';
 
 const Projects = () => {
     return (
         <div className='projects'>
-            데이터 만들어서 넣어야함.
+            
         </div>
     );
 };
