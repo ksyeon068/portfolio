e5896b6815c0c738f139db12497e3be1d37f2301
Kim Seyeon <ksyeon068@gmail.com>
2026-04-11 16:20:22 +0900

    로딩폴더위치변경

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import Loading from './components/loading/Loading'
+import Loading from './components/Loading'
 
 
 import './App.css'
